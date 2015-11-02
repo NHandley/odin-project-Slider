5171cdc38500cc6b5d2041829c2084d3fd4f3b8e
Nathan Handley <nathan.handley1@gmail.com>
2015-11-02 21:20:17 +1100

    Logic update
    Added previous button

--- slide.js
+++ slide.js
@@ -1,23 +1,37 @@
 (function() {
 
 	// variables
-	var count = 1;
-	var sliderLength = 4;
-	var delay = 2500;
+	var count = 1, sliderLength = 4, delay = 2500;
+	var firstSlide = $('#slide1'), lastSlide = $('#slide4');
 
-	$('button').click(function() {		
 
-		$("#slide"+count).addClass("active").hide("slide", { direction: "left" }, 1000);
+	$('#next').click(function() {		
 		
 		if(count > sliderLength){
 			count = 1;
 		}
 
-		//$("#slide"+count).addClass("active").slideToggle("slow");
+		/*if (count > 1) {
+			$("#slide"+count).addClass("active").hide("slide", { direction: "left" }, 1000);
+		}*/
+
 		$("#slide"+count).addClass("active").show("slide", { direction: "right" }, 1000);
-		
-		
+			
 			count++;
+			console.log(count);
+	});
+
+		$('#prev').click(function() {		
+		
+
+		if (count <= 1) {
+			count = 4;
+		};
+
+		$("#slide"+count).addClass("active").show("slide", { direction: "left" }, 1000);
+		
+			count--;
+			console.log(count);
 
 	});
 
