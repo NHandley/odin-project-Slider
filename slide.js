5dcfaf697a12ad7da721f7bdbe68339f1a9fb363
Nathan <NHandley@users.noreply.github.com>
2015-11-10 10:45:59 +1100

    Update fixes to next button

--- slide.js
+++ slide.js
@@ -1,34 +1,35 @@
 (function() {
 
 	// variables
-	var count = 1, sliderLength = 4, delay = 2500;
-	var firstSlide = $('#slide1'), lastSlide = $('#slide4');
+	var count = 2, sliderLength = 4, delay = 2500;
 
+	$("#slide1").show("fade", 1000);
 
+	//Images stop sliding after count is reset
 	$('#next').click(function() {		
 		
 		if(count > sliderLength){
 			count = 1;
 		}
 
-		/*if (count > 1) {
-			$("#slide"+count).addClass("active").hide("slide", { direction: "left" }, 1000);
-		}*/
-
-		$("#slide"+count).addClass("active").show("slide", { direction: "right" }, 1000);
+		//Not sure how to hide the last slide when count is reset and back to first image
+		$("#slide"+(count - 1)).hide("slide", { direction: "left" }, 1000);	
+		$("#slide"+count).show("slide", { direction: "right" }, 1000);
 			
-			count++;
 			console.log(count);
+			count++;
 	});
 
+		//Will fix once solved issues for next button
 		$('#prev').click(function() {		
 		
 
-		if (count <= 1) {
+		if (count < 1) {
 			count = 4;
 		};
 
-		$("#slide"+count).addClass("active").show("slide", { direction: "left" }, 1000);
+		$("#slide"+(count + 1)).hide("slide", { direction: "right" }, 1000);
+		$("#slide"+count).show("slide", { direction: "left" }, 1000);
 		
 			count--;
 			console.log(count);
