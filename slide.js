9b4ceeef3dce89ebea2058ec8db0e42cf243fca0
NHandley <nathan.handley1@gmail.com>
2015-11-25 20:00:57 +1100

    Previous button now working correctly
    Finally working the way I want :)

--- slide.js
+++ slide.js
@@ -1,40 +1,32 @@
 (function() {
 
 	// variables
-	var count = 2, sliderLength = 4, delay = 2500;
+	var count = 1, sliderLength = 4, delay = 2500;
 
 	$("#slide1").show("fade", 1000);
 
 	setInterval(function() {
-
-		if(count > sliderLength){
-		$("#slide"+(count - 1)).hide("slide", { direction: "left" }, 1000);
-		count = 1;
-		}
-
-		$("#slide"+(count - 1)).hide("slide", { direction: "left" }, 1000);	
-		$("#slide"+count).show("slide", { direction: "right" }, 1000);
-			
-		console.log(count);
-		count++;
-
+		$('#next').trigger("click");
+		
 	}, 3000);
 
 	$('#next').click(function() {		
 		
+
+		$("#slide"+count).hide("slide", { direction: "left" }, 1000);
+		count++;
+
 		if(count > sliderLength){
-			$("#slide"+(count - 1)).hide("slide", { direction: "left" }, 1000);
 			count = 1;
 		}
 
-		$("#slide"+(count - 1)).hide("slide", { direction: "left" }, 1000);	
 		$("#slide"+count).show("slide", { direction: "right" }, 1000);
 			
 			console.log(count);
-			count++;
+			
 	});
 
-		$('#prev').click(function() {		
+	$('#prev').click(function() {
 		
 		$("#slide"+count).hide("slide", { direction: "right" }, 1000);
 		count--;
